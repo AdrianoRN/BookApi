7fcc2d8718a6dcc5b4e743da60fcb7dd2e2c5858
Adriano Ribeiro <adrianoribeiro589@gmail.com>
2022-06-12 23:04:40 -0300

    Adicionar arquivos de projeto.

---
+++ BookApi/Controllers/BooksController.cs
@@ -0,0 +1,13 @@
+﻿using BooksAPI.Repositories;
+
+namespace BookApi.Controllers
+{
+    public class BooksController
+    {
+        private readonly IBookRepository _bookRepository;
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+    }
+}
